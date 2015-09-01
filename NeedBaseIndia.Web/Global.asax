7d4d412baf5566805739af21ae77fdce745d6ba9
Mukesh purohit <mpurohit88@live.com>
2015-09-01 17:09:04 +0530

    Initial Check in.

---
+++ NeedBaseIndia.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NeedBaseIndia.Web.MvcApplication" Language="C#" %>
---
+++ NeedBaseIndia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NeedBaseIndia.Startup))]
+namespace NeedBaseIndia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
